3a49c7fd3490e0d7a5522dd44a3c51d3d1d3d207
Mahan Navidu <mahan.navidu@gmail.com>
2020-02-11 01:19:28 -0800

    Pod Initialization

---
+++ WikiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiSearch
+//
+//  Created by Mahan Malporu on 2/11/20.
+//  Copyright © 2020 Mahan Malporu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
